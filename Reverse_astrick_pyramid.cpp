d6ec14a8b3ce9b3c68f09032ed27f8e31358024d
MilanLaddha <160130369+MilanLaddha@users.noreply.github.com>
2024-07-08 00:56:16 +0530

    Update Reverse_astrick_pyramid.cpp

--- Reverse_astrick_pyramid.cpp
+++ Reverse_astrick_pyramid.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-include namespace std;
+using namespace std;
 int main(){
-  int n,i,j
-  cout<<"enter the numer for revere pyramid"
-  cin>>n
+  int n,i,j;
+  cout<<"enter the numer for revere pyramid";
+  cin>>n;
   for(i=n;i=1;i--){
-    for(j=1;j=1;j--){
+    for(j=i;j=1;j--){
+      cout<<"*";
+    }
+    cout<<endl;
   }
+  return 0;
 }
+
